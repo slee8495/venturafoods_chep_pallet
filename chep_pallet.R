40c26b4d6e91a1d6d73f65c4038cf5cef74a663c
slee <sanlee8495@gmail.com>
2023-10-09 17:07:11 -0700

    10/09/2023

---
+++ chep_pallet.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(magrittr)
+library(openxlsx)
+library(readxl)
+library(writexl)
+library(reshape2)
+library(skimr)
+library(janitor)
+library(lubridate)
+
+
+##################################################################################################################################################################
+gtl_raw <- read_csv("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/CHEP Pallet/GTL.5018077265_20230812_6100788687_20230813_124450.csv")
+as400_raw <- read_csv("C:/Users/slee/OneDrive - Ventura Foods/Ventura Work/SCE/Project/FY 24/CHEP Pallet/EF065565.csv")
+
+##################################################################################################################################################################
+
+
+# gtl (CHEP data) clean
+gtl_raw[-1:-13, ] -> gtl
+colnames(gtl) <- gtl[1, ]
+
+gtl %>% 
+  dplyr::slice(-1) %>% 
+  janitor::clean_names() %>% 
+  dplyr::mutate(dplyr::across(contains("date"), ~as.Date(.x, format="%d.%m.%Y"))) -> gtl
+
+
+# as400 data clean
+colnames(as400_raw) <- as400_raw[1, ]
+as400_raw %>% 
+  dplyr::slice(-1) %>% 
+  janitor::clean_names() %>% 
+  dplyr::mutate(dplyr::across(contains("date"), ~as.Date(.x, format="%m/%d/%Y"))) -> as400
+
+
+##################################################################################################################################################################
+
+
+
+
+
+
+
