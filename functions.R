b176e0ad3055caf16443218aa576c249a2ddd37a
slee <sanlee8495@gmail.com>
2024-03-28 11:27:06 -0700

    03282024

--- functions.R
+++ functions.R
@@ -113,8 +113,9 @@ chep_cleaning <- function(chep, chep_ref) {
     janitor::clean_names() %>% 
     dplyr::rename(plt_qty = quantity, reference_2 = reference2, reference_3 = reference3) %>%
     dplyr::select(sender_name, receiver_name, plt_qty, reference_2, reference_3) %>% 
-    dplyr::mutate(reference_3 =  as.numeric(gsub("\\D", "", reference_3))) %>% 
-    dplyr::mutate(reference_3 = ifelse(is.na(reference_3), as.numeric(gsub("\\D", "", reference_2)), reference_3)) %>% 
+    # dplyr::mutate(reference_3 =  as.numeric(gsub("\\D", "", reference_3))) %>% 
+    
+    dplyr::mutate(reference_3 = ifelse(is.na(reference_3), as.numeric(gsub("\\D", "", reference_2)), reference_3)) %>%
     dplyr::mutate(ship_location = stringr::str_sub(reference_2, 1, nchar(reference_2) -5),
                   bill_of_lading = stringr::str_sub(reference_2, -5)) %>% 
     left_join(chep_ref) %>%
@@ -150,8 +151,9 @@ chep_cleaning_2 <- function(chep) {
     janitor::clean_names() %>% 
     dplyr::rename(plt_qty = quantity, reference_2 = reference2, reference_3 = reference3) %>%
     dplyr::select(sender_name, receiver_name, plt_qty, reference_2, reference_3) %>% 
-    dplyr::mutate(reference_3 =  as.numeric(gsub("\\D", "", reference_3))) %>% 
-    dplyr::mutate(reference_3 = ifelse(is.na(reference_3), as.numeric(gsub("\\D", "", reference_2)), reference_3)) %>% 
+    # dplyr::mutate(reference_3 =  as.numeric(gsub("\\D", "", reference_3))) %>% 
+    dplyr::mutate(reference_3 = ifelse(is.na(reference_3), as.numeric(gsub("\\D", "", reference_2)), reference_3)) %>%
+    
     dplyr::mutate(ship_location = stringr::str_sub(reference_2, 1, nchar(reference_2) -5),
                   bill_of_lading = stringr::str_sub(reference_2, -5)) %>% 
     rename(customer_po_number = reference_3) %>%
